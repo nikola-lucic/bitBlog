ed92aea9232df3efeefdbd06f6c82a1b63816fe8
BIT Student <classroom@bgit.rs>
2018-03-23 18:19:20 +0100

    partials feature components staging

---
+++ bit-blog/src/app/partials/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const Footer = (props) => {
+    return (
+
+        <footer class="page-footer blue">
+            <div class="container">
+                <div class="row">
+                    <div class="col s12 m12 l12">
+                        <div class="footer-copyright">
+                            <div class="container">
+                                © 2018 Bit Blog
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
