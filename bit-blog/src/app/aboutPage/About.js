d3aac77142c6285b0c3c8aa2e128781bb81ed8db
BIT Student <classroom@bgit.rs>
2018-03-26 10:57:52 +0200

    about page content

---
+++ bit-blog/src/app/aboutPage/About.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import AboutContent from "./AboutContent";
+
+const About = (props) => {
+  return <AboutContent />;
+};
+
+export default About;
---
+++ bit-blog/src/app/aboutPage/AboutContent.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+const AboutContent = (props) => {
+  return (
+    <main className="container">
+      <div className="row left-align">
+        <div className="col s12 m12 l12" />
+        <h1 className="center-align">About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, quo ea nonumes assueverit, admodum
+          noluisse definiebas est cu. Sonet tollit et vel, usu ei graeco
+          tacimates. Id his invidunt disputando. Te falli facilis pri, an his
+          vitae constituam, mei eu oratio postulant. Corpora corrumpit assentior
+          est et, alii tractatos sed te, cibo tritani constituto in eam. Ei usu
+          eruditi definitionem, eu vis agam deseruisse. Nec an munere possim,
+          pro error aeterno singulis et. Quo ludus percipit cu, his te cetero
+          torquatos voluptatum. In vis hinc omittam molestiae. Ius vide soleat
+          ubique ne. Ex augue electram sapientem vim, sit rebum dolores legendos
+          ne, mundi cotidieque eam et. Ne vim legere facete, case senserit
+          salutatus sed te. Eu rebum vocent his. Tale essent detraxit usu ad,
+          quod facer persequeris ut mel. Zril accumsan te vim, usu cu ridens
+          dissentiet, est eu maluisset efficiendi. Alterum repudiare adolescens
+          sea at. Vero viderer malorum ut nam, cum cu equidem incorrupte. Te qui
+          quod comprehensam. Laudem euripidis mel ne. Dolorem menandri petentium
+          has in, at vel atqui corpora. Eam in iuvaret percipit interesset. Ad
+          vide probatus pro, eam ignota impetus ex. Elit splendide delicatissimi
+          sit in, eos decore exerci quidam id. In tantas nostro eos. Per ad quas
+          natum. Vis et tempor noster propriae, regione tritani ea mei, nam
+          dolorum omnesque appetere ei. Duo consul sententiae et, quo idque
+          labitur delenit ne, sed fugit saperet eu. Lorem ipsum dolor sit amet,
+          quo ea nonumes assueverit, admodum noluisse definiebas est cu. Sonet
+          tollit et vel, usu ei graeco tacimates. Id his invidunt disputando. Te
+          falli facilis pri, an his vitae constituam, mei eu oratio postulant.
+          Corpora corrumpit assentior est et, alii tractatos sed te, cibo
+          tritani constituto in eam. Ei usu eruditi definitionem, eu vis agam
+          deseruisse. Nec an munere possim, pro error aeterno singulis et. Quo
+          ludus percipit cu, his te cetero torquatos voluptatum. In vis hinc
+          omittam molestiae. Ius vide soleat ubique ne. Ex augue electram
+          sapientem vim, sit rebum dolores legendos ne, mundi cotidieque eam et.
+          Ne vim legere facete, case senserit salutatus sed te. Eu rebum vocent
+          his. Tale essent detraxit usu ad, quod facer persequeris ut mel. Zril
+          accumsan te vim, usu cu ridens dissentiet, est eu maluisset
+          efficiendi. Alterum repudiare adolescens sea at. Vero viderer malorum
+          ut nam, cum cu equidem incorrupte. Te qui quod comprehensam. Laudem
+          euripidis mel ne. Dolorem menandri petentium has in, at vel atqui
+          corpora. Eam in iuvaret percipit interesset. Ad vide probatus pro, eam
+          ignota impetus ex. Elit splendide delicatissimi sit in, eos decore
+          exerci quidam id. In tantas nostro eos. Per ad quas natum. Vis et
+          tempor noster propriae, regione tritani ea mei, nam dolorum omnesque
+          appetere ei. Duo consul sententiae et, quo idque labitur delenit ne,
+          sed fugit saperet eu.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default AboutContent;
