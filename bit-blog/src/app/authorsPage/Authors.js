e9afc90be72fc99b2ee5dc6ab032071b475b0ad3
BIT Student <classroom@bgit.rs>
2018-03-26 10:44:25 +0200

    authorsPage components

--- bit-blog/src/app/authorsPage/Authors.js
+++ bit-blog/src/app/authorsPage/Authors.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import AuthorsLists from "./AuthorsList";
 
-const Authors = props => {
-  return (
-    <main className="conatiner">
+class Authors extends React {
+  render() {
+    return (
+      <main className="container">
       <h1 className="align-center">Authors</h1>
       <AuthorsLists />
     </main>
-  );
-};
-
+    )
+  }
+}
 export default Authors;
